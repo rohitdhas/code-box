39deb508521facd284954ca87f0b970066c2f82e
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-21 14:13:48 +0530

    projects api works ✨

---
+++ pages/api/projects.js
@@ -0,0 +1,26 @@
+import { connectToDatabase } from "../../lib/mongodb";
+import { getCookieVal } from "../../utils";
+
+export default async function handler(req, res) {
+  const { userId } = req.query;
+
+  let { db } = await connectToDatabase();
+  const userToken = getCookieVal(req.headers.cookie, "codebox-token");
+  const tokenCheck = await db
+    .collection("tokens")
+    .findOne({ token: userToken });
+
+  if (!userToken || !tokenCheck) {
+    res.status(404).json({
+      isError: true,
+      message: "Something went wrong! Please relogin!",
+    });
+    return;
+  }
+
+  const projects = await db
+    .collection("projects")
+    .find({ user: userId })
+    .toArray();
+  res.status(200).json({ isError: false, data: projects });
+}
