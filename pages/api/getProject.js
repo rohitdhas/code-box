f25887f64006fc7663ca4c57722565b28a0a3f42
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-23 16:27:33 +0530

    get project api update

--- pages/api/getProject.js
+++ pages/api/getProject.js
@@ -1,9 +1,17 @@
 import { connectToDatabase } from "../../lib/mongodb";
+import { ObjectId } from "mongodb";
 
 export default async function handler(req, res) {
   const { projectId } = req.query;
   let { db } = await connectToDatabase();
 
-  const doc = await db.collection("projects").find({ _id: projectId });
+  if (!ObjectId.isValid(projectId)) {
+    res.status(400).json({ isError: true, message: "Project not found!" });
+    return;
+  }
+
+  const doc = await db
+    .collection("projects")
+    .findOne({ _id: ObjectId(projectId) });
   res.status(200).json({ isError: false, data: doc });
 }
