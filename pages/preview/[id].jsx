0a338643134e75e1cb5354af456e8082b7e6df06
Rohit Dhas <rohitdhas666@gmail.com>
2022-07-23 16:25:33 +0530

    project preview works 🔥🚀

---
+++ pages/preview/[id].jsx
@@ -0,0 +1,54 @@
+import { useRouter } from "next/router";
+import Nav from "../../components/navbar";
+import Loader from "../../components/loader";
+import { useState, useEffect } from "react";
+
+export default function Preview() {
+  const router = useRouter();
+  const { id } = router.query;
+  const [srcDoc, setSrcDoc] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProject();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const getProject = async () => {
+    setIsLoading(true);
+
+    const res = await fetch(`/api/getProject?projectId=${id}`);
+    const { isError, data } = await res.json();
+    setIsLoading(false);
+
+    if (isError) {
+      router.push({ pathname: "/" });
+    } else {
+      const { name, html, css, js } = data;
+      document.title = `Codebox Preview - ${name}`;
+      setSrcDoc(`
+      <html>
+        <body>${html}</body>
+        <style>${css}</style>
+        <script>${js}</script>
+      </html>
+    `);
+    }
+  };
+
+  return (
+    <div>
+      <Nav />
+      <Loader isLoading={isLoading} />
+      <iframe
+        srcDoc={srcDoc}
+        title="output"
+        sandbox="allow-scripts"
+        frameBorder="0"
+        width="100%"
+        height="100%"
+        className="h-[90vh]"
+      />
+    </div>
+  );
+}
